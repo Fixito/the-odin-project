29b74966b1aab82de4a50eb35bbbefc441dd106d
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-08-10 13:31:18 +0200

    add output management in webpack-demo

--- 02-full-stack-javascript/wepback-demo/src/index.js
+++ 02-full-stack-javascript/wepback-demo/src/index.js
@@ -1,11 +1,13 @@
 import _ from 'lodash';
 import './style.css';
 import Img from './image.jpg';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.textContent = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
 
   // Add the image to our existing div.
@@ -13,6 +15,12 @@ function component() {
   myImage.src = Img;
 
   element.appendChild(myImage);
+
+  // Add the btn t our existing div
+  btn.textContent = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
