2b77162d82f83beb5dc462b2878f795b9015b4e3
Thomas ROBERT <thomas_3004@hotmail.fr>
2022-10-27 16:15:37 +0200

    Complete prototype

--- 01-foundations/03-rock-paper-scissors/app.js
+++ 01-foundations/03-rock-paper-scissors/app.js
@@ -1,5 +1,21 @@
-const choices = ['pierre', 'papier', 'ciseaux'];
-const NUMBER_OF_ROUNDS = 5;
+const choices = [
+  { hand: 'rock', class: 'fas fa-hand-back-fist' },
+  { hand: 'paper', class: 'fas fa-hand' },
+  { hand: 'scissors', class: 'fas fa-hand-peace' }
+];
+
+const btns = document.querySelectorAll('.btn');
+const playAgainBtn = document.querySelector('.play-again');
+const results = document.querySelector('.results');
+const humanSign = document.querySelector('.human-sign');
+const humanScore = document.querySelector('.human-score');
+const cpuSign = document.querySelector('.cpu-sign');
+const cpuScore = document.querySelector('.cpu-score');
+const comment = document.querySelector('.comment');
+let score = {
+  human: 0,
+  cpu: 0
+};
 
 const getComputerChoice = () => {
   return choices[Math.floor(Math.random() * choices.length)];
@@ -7,54 +23,64 @@ const getComputerChoice = () => {
 
 const playRound = (playerSelection, computerSelection) => {
   if (playerSelection === computerSelection) {
-    console.log("C'est une égalité");
     return 0;
   } else if (
-    (playerSelection === 'pierre' && computerSelection === 'ciseaux') ||
-    (playerSelection === 'papier' && computerSelection === 'pierre') ||
-    (playerSelection === 'ciseaux' && computerSelection === 'papier')
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
-    console.log('Vous avez gagnez cette manche');
     return 1;
   } else {
-    console.log('Vous avez perdez cette manche');
     return -1;
   }
 };
 
-const game = () => {
-  let score = 0;
-
-  for (let i = 0; i < NUMBER_OF_ROUNDS; i++) {
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    const playerSelection = e.currentTarget.dataset.selection;
     const computerSelection = getComputerChoice();
-    let playerSelection;
+    const iconClassList = e.currentTarget.children[0].classList.value;
+    const result = playRound(playerSelection, computerSelection.hand);
+    let sentence = '';
 
-    do {
-      playerSelection = prompt(
-        'Entrez une valeur parmi : pierre, papier ou ciseaux'
-      );
+    humanSign.children[0].className = iconClassList;
+    cpuSign.children[0].className = computerSelection.class;
 
-      if (!playerSelection || playerSelection === null) {
-        return;
+    if (result === 0) {
+      sentence = `C'est une égalité.`;
+    } else if (result > 0) {
+      score.human++;
+      humanScore.textContent = score.human;
+      sentence = `Vous gagnez cette manche.`;
+    } else {
+      score.cpu++;
+      cpuScore.textContent = score.cpu;
+      sentence = `Vous perdez cette manche.`;
+    }
+
+    if (score.human >= 5 || score.cpu >= 5) {
+      if (score.human >= 5) {
+        sentence = 'Vous remportez la partie !';
       } else {
-        playerSelection = playerSelection.toLowerCase();
+        sentence = "L'ordinateur remporte la partie !";
       }
-    } while (
-      playerSelection != 'pierre' &&
-      playerSelection != 'papier' &&
-      playerSelection != 'ciseaux'
-    );
 
-    score += playRound(playerSelection, computerSelection);
-  }
+      playAgainBtn.style.display = 'block';
+    }
 
-  if (score === 0) {
-    console.log('Math nul.');
-  } else if (score > 0) {
-    console.log('Vous remportez la partie !');
-  } else {
-    console.log('Vous perdez la partie :(');
-  }
-};
+    comment.textContent = sentence;
+  });
+});
 
-game();
+playAgainBtn.addEventListener('click', () => {
+  score = {
+    human: 0,
+    cpu: 0
+  };
+  humanSign.children[0].className = 'fas fa-question';
+  cpuSign.children[0].className = 'fas fa-question';
+  humanScore.textContent = 0;
+  cpuScore.textContent = 0;
+  comment.textContent = '';
+  playAgainBtn.style.display = 'none';
+});
